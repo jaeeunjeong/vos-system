9c24540085bfa1387a10405d6c0d5d63a2ae049d
jaeeunjeong <jejeong000@gmail.com>
2022-07-06 23:26:23 +0900

    Initial commit

---
+++ src/main/java/com/teamfresh/vossystem/VosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.teamfresh.vossystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VosSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamfresh/vossystem/VosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamfresh.vossystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
